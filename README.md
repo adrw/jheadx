fe37464b912f866f96475ed63dfc3d220bce7cef
Andrew Paradi <account+github@adrw.ch>
2018-08-18 22:43:14 -0400

    Initial script structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# photo-exif-fake-timeline
-Create fake EXIF timestamp data with sequentially increasing times on all photos in a directory
+# jheadx
+
+`jhead` wrapper with extended functionality to batch fix with photos lacking EXIF data
+
+# Added Commands
+- `$ jheadx -xr {sourceDir} {restoreDir}`: jhead**x** **r**estores EXIF from files in the source directory to any matching filenames on the restore directory
+- `$ jheadx -xf {sourceDir} {startDateTime} {endDateTime}`: jhead**x** **f**akes timestamps in a directory of photos. It loops over input files setting EXIT timestamp and file modification timestamp to a sequentially increasing time linearly distributed between `startDateTime` and `endDateTime`. Helpful for restoring fake EXIF timestamps that mirror in timestamps the sorted-by-name ordering of photo files in a source directory. This provides a fake timeline that makes use of photos in libraries easier since they remain in order and are approximately accurate to the date or time block that they were taken in.
+
+# Resources
+- [jhead](http://www.sentex.net/~mwandel/jhead/)
---
+++ jheadx.sh
@@ -0,0 +1,109 @@
+# By Andrew Paradi | Source at https://github.com/adrw/jheadx
+#!/usr/bin/env bash
+
+# set up bash to handle errors more aggressively - a "strict mode" of sorts
+set -e # give an error if any command finishes with a non-zero exit code
+set -u # give an error if we reference unset variables
+set -o pipefail # for a pipeline, if any of the commands fail with a non-zero exit code, fail the entire pipeline with that exit code
+
+function status() {
+  Reset="$(tput sgr0)"       # Text Reset
+  Red="$(tput setaf 1)"          # Red
+  Green="$(tput setaf 2)"        # Green
+  Blue="$(tput setaf 4)"         # Blue
+  div="********************************************************************************"
+  if [ "$#" -lt 3 ]; then   # if no name override passed in, take name "ap" if $0 is status, $0 otherwise
+    [ $(basename "${0}") = "status" ] && scriptname="ap" || scriptname=$(basename "${0}")
+  else
+    scriptname="${3}"
+  fi
+  case "${1}" in
+    a)        echo ""; echo "${Blue}<|${scriptname:0:1}${Reset} [ ${2} ] ${div:$((${#2}+9))}" ;;
+    b)        echo "${Green}ok: [ ${2} ] ${div:$((${#2}+9))}${Reset}" ;;
+    s|status) echo "${Blue}<|${scriptname:0:1}${Reset} [ ${2} ] ${div:$((${#2}+9))}" ;;
+    t|title)  echo "${Blue}<|${scriptname}${Reset} [ ${2} ] ${div:$((${#2}+8+${#scriptname}))}" ;;
+    e|err)    echo "${Red}fatal: [ ${2} ] ${div:$((${#2}+12))}${Reset}" ;;
+  esac
+}
+
+function show_help {
+cat << EOF
+❓  Usage :: jheadx <opts>
+
+-h    Show help menu
+
+-xf {sourceDir} {startDateTime} {endDateTime}
+
+    Fake timestamps uniformly across directory of files
+
+    sourceDir: 
+    startDateTime:
+    endDateTime:
+
+-xr {sourceDir} {restoreDir}
+
+    Restore timestamps to matching filenames from a source directory
+
+    sourceDir:
+    restoreDir:
+EOF
+  exit 0
+}
+
+function fake_timestamps {
+  echo "func: fake timestamps"
+  echo "not built yet, exiting"
+  exit 0
+
+  startDateTime=$1
+  directory=$2
+  minuteDifference=$3
+
+  echo $startDateTime $directory $minuteDifference
+
+  currentHour=$(date "%Y:%m:%d/%H:%M:%S" $startDateTime +"%H")
+  currentMinute=$(date "%Y:%m:%d/%H:%M:%S" $startDateTime +"%M")
+
+  for photo in $directory; do
+    # create basic empty header with thumbnail
+    # jhead -mkexif -rgt $photo
+    # 4
+    # jhead -ts
+    echo $currentHour $currentMinute
+    # nextMinute=$(( $currentMinute + $minuteDifference))
+    # [ nextMinute -gt 60 ] && currentHour++ || currentMinute=$nextMinute
+  done
+  exit 0
+}
+
+function restore_timestamps {
+  echo "func: restore timestamps"
+
+  exit 0
+}
+
+CMD_X=false
+CMD_F=false
+CMD_R=false
+
+while getopts "h?xrfabcdegijklmnopqstuvwyz" opt; do
+  case "$opt" in
+  h)  show_help
+      exit 0
+      ;;
+  x)  echo "CMD_X"
+      CMD_X=true
+      ;;
+  f)  [ "$CMD_X" = true ] && CMD_F=true ;;
+  r)  [ "$CMD_X" = true ] && CMD_R=true ;;
+  *)  echo "$ jhead $@"
+      exit 0
+      ;;
+  esac
+done
+
+[ "$CMD_X" = true ] && [ "$CMD_F" = true ] && echo "-xf   fake timestamps" && fake_timestamps
+[ "$CMD_X" = true ] && [ "$CMD_R" = true ] && echo "-xr   restore timestamps" && restore_timestamps
+
+echo "No matching command in jhead or jheadx."
+exit 1
