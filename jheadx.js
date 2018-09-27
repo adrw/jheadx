2bd5f3a0bf9e628ba1681d5ed5e5be04d1fb703a
Andrew Paradi <account+github@adrw.ch>
2018-09-27 00:03:09 -0400

    progress using commander framework

--- jheadx.js
+++ jheadx.js
@@ -1,6 +1,27 @@
 #!/usr/bin/env node
-(function () {
-  "use strict";
 
-  
-})
+"use strict"
+
+const program = require("commander")
+const dayjs = require("dayjs")
+
+program
+  .version('0.0.1', '-v, --version')
+  .option('-x, --extended', 'jheadx extended option prefix')
+  .option('-f, --fake', 'fake timestamps')
+  .option('-r, --restore', 'restore timestamps')
+  .parse(process.argv)
+
+if (program.extended) {
+  if (program.fake) {
+    console.log("fake") 
+
+  } else if (program.restore) {
+    console.log("restore") 
+  }
+} else {
+  console.log("$ jhead", process.argv[2])
+}
+
+// program
+//   .parseOptions(process.argv)
