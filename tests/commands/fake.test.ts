a3327359c0cc1d5cc3c4ae1bd90abf4bbda8cf67
Andrew Alexander <account+github@adrw.ch>
2019-01-28 22:31:34 -0500

    Jest WIP

--- tests/commands/fake.test.ts
+++ tests/commands/fake.test.ts
@@ -6,14 +6,14 @@ import * as utils from "../../src/utils"
 jest.mock("inquirer/lib/utils/screen-manager")
 jest.mock("../../src/utils")
 
-const logInfo = jest.spyOn(utils, "logger")
+const executeInfo = jest.spyOn(utils, "execute")
 const renderQuestion = jest.spyOn(
   require("inquirer/lib/utils/screen-manager").prototype,
   "render"
 )
 
 afterEach(() => {
-  logInfo.mockReset()
+  executeInfo.mockReset()
   renderQuestion.mockReset()
 })
 
@@ -21,9 +21,9 @@ describe("fake", () => {
   it("says hello with the right name, after asking it if not provided", async () => {
     await runWithAnswers(() => fake(), ["-d", ENTER])
     expect(renderQuestion.mock.calls[0][0]).toMatchSnapshot()
-    expect(logInfo).toHaveBeenCalledTimes(2)
-    expect(logInfo.mock.calls[0][0]).toMatchSnapshot()
-    expect(logInfo.mock.calls[1][0]).toMatchSnapshot()
+    expect(executeInfo).toHaveBeenCalledTimes(2)
+    expect(executeInfo.mock.calls[0][0]).toMatchSnapshot()
+    expect(executeInfo.mock.calls[1][0]).toMatchSnapshot()
   })
 
   // it("says fake with the right name, without asking it if provided", async () => {
